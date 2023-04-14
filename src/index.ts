70239e5c40875cb448aca35a222c7fa3e1859a06
drakeerv <drakeerv@outlook.com>
2023-04-14 00:31:17 -0400

    Add env and better dev

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,42 @@
-// import fastify and set up socket.io
 import fastify from "fastify";
 import fastifySocketio from "fastify-socket.io";
+import dotenv from "dotenv";
 
+function randomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+dotenv.config();
+
+const LOGGING = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
 const PORT = Number(process.env.PORT) || 3000;
 const ADDRESS = process.env.ADDRESS || "0.0.0.0";
 
-const app = fastify();
+const app = fastify({
+  logger: LOGGING[process.env.ENVIRONMENT] ?? true,
+});
 
 app.register(fastifySocketio);
 
-const start = async () => {
-  try {
-    await app.listen({ port: PORT, host: ADDRESS });
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-};
-start();
+process.on("beforeExit", async () => {
+  await app.close();
+})
+
+app.listen({ port: PORT, host: ADDRESS, exclusive: true }).catch((err) => {
+  app.log.error(err);
+  process.exit(1);
+});
+
+// https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
